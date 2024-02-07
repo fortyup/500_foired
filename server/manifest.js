ad1135f27c0cb3255003b84bbcbf125341b21fc6
CAPEL Maxime <83071634+fortyup@users.noreply.github.com>
2024-02-07 17:22:01 +0100

    Initial commit

---
+++ server/manifest.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('@hapipal/confidence');
+const Toys = require('@hapipal/toys');
+const Schwifty = require('@hapipal/schwifty');
+
+// Pull .env into process.env
+Dotenv.config({ path: `${__dirname}/.env` });
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: 'localhost',
+        port: {
+            $env: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: { $env: 'NODE_ENV' },
+            $default: {
+                log: ['error'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {}
+            },
+            {
+                plugin: './plugins/swagger'
+            },
+            {
+                plugin: '@hapipal/schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'mysql',
+                            connection: {
+                                host: process.env.DB_HOST || '0.0.0.0',
+                                user: process.env.DB_USER || 'fortyup',
+                                password: process.env.DB_PASSWORD || 'hapi',
+                                database: process.env.DB_DATABASE || 'user',
+                                port: process.env.DB_PORT || 3308
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
+            },
+            {
+                plugin: {
+                    $filter: { $env: 'NODE_ENV' },
+                    $default: '@hapipal/hpal-debug',
+                    production: Toys.noop
+                }
+            }
+        ]
+    }
+});
